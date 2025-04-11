82d2f738b1a9cc53e5df337c6a61744fc96b1b81
Sk Sabiruddin <64342268+sabirproduct@users.noreply.github.com>
2025-04-11 02:01:30 +0000

    modified settings

---
+++ DataAPI/index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('🚀 Hello from Express API on Render!');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello world!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
